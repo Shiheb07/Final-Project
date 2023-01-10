0ecb1f3a4cace411ec7acd2f25eea69effeab6b1
Shiheb <shiheb007@gmail.com>
2023-01-10 17:29:52 +0100

    carrousel 1

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,9 +1,18 @@
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
-function myFunction() {
-    // document.getElementById("bgCarrousel").style.backgroundImage = url('../images/bg.jpg');
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-    document.getElementById("carrouselId").fadeTo('slow', 0.3, function () {
-        $(this).css('background-image', "url('../images/bg.jpg')");
-    }).delay(1000).fadeTo('slow', 1)
-}
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
