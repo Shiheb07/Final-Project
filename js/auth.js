56e76f3c971b0072231cdae91886f45979460d17
Shiheb <shiheb007@gmail.com>
2023-01-13 05:54:01 +0100

    90%

--- js/auth.js
+++ js/auth.js
@@ -7,13 +7,14 @@ $(document).ready(function() {
 let username = document.getElementById("InputUsername").value ;
 let password = document.getElementById("InputPassword").value ;
 if (username === ""){
-    document.getElementById("message").innerHTML = "Username is missing!";
+    document.getElementById("message").innerHTML = " Username is missing!";
     $('#alertMessage').show();
 } else if (password === ""){
-    document.getElementById("message").innerHTML = "Password is missing!";
+    document.getElementById("message").innerHTML = " Password is missing!";
     $('#alertMessage').show();
 } else {
     $('#alertMessage').hide();
+    window.location.href="/home.html"
 }
 
 }
@@ -22,5 +23,27 @@ function signUp() {
     let email = document.getElementById("inputEmailId").value ;
     let password = document.getElementById("inputPassword").value ;
     let retryPassword = document.getElementById("inputRetryPassword").value ;
+    if (name === ""){
+        document.getElementById("messageErreur").innerHTML = " Name is missing!";
+        $('#alertMessage').show();
+    } else if (email === ""){
+        document.getElementById("messageErreur").innerHTML = " Email is missing!";
+        $('#alertMessage').show();
+    } else if  (password === ""){ 
+        document.getElementById("messageErreur").innerHTML = " Password is missing!";
+        $('#alertMessage').show();
+    } else if (retryPassword === ""){
+        document.getElementById("messageErreur").innerHTML = " Password Cofirmation is missing!";
+        $('#alertMessage').show();
+    } else if (! email.includes("@")){
+    document.getElementById("messageErreur").innerHTML = " invalid Email!";
+    }  else if (password !== retryPassword ) {
+        document.getElementById("messageErreur").innerHTML = " Wrong Password Confirmation!";
+    } else {
+        $('#alertMessage').hide();
+        window.location.href="/home.html"
+    }
 
+
+    
 };
